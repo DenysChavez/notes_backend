3b4992fbe9136963394157412053db8f80bcb67b
Denys Chavez <dvchf26@gmail.com>
2024-07-11 17:29:46 -0400

    put the test of reverse funtion inside of decribe so that the test report output gets grouped nicely

--- tests/reverse.test.js
+++ tests/reverse.test.js
@@ -1,21 +1,27 @@
-const { test } = require("node:test");
+const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const reverse = require("../utils/for_testing").reverse;
 
-test("reverse of a", () => {
-  const result = reverse("a");
+describe("reverse", () => {
 
-  assert.strictEqual(result, "a");
-});
+  test("reverse of a", () => {
+    const result = reverse("a");
 
-test("reverse of react", () => {
-  const result = reverse("react");
+    assert.strictEqual(result, "a");
+  });
 
-  assert.strictEqual(result, "tcaer");
-});
+  test("reverse of react", () => {
+    const result = reverse("react");
 
-test("reverse of saippuakauppias", () => {
-  const result = reverse("saippuakauppias");
+    assert.strictEqual(result, "tcaer");
+  });
 
-  assert.strictEqual(result, "saippuakauppias");
-});
+  test("reverse of saippuakauppias", () => {
+    const result = reverse("saippuakauppias");
+
+    assert.strictEqual(result, "saippuakauppias");
+  });
+
+})
+
+
